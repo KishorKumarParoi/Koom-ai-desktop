9381810a1adbe9691dc2c10dafe34b8e89339efd
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-08-04 23:29:10 +0600

    Update webcam component

--- src/components/global/webcam/index.tsx
+++ src/components/global/webcam/index.tsx
@@ -1,5 +1,30 @@
+import { useEffect, useRef } from "react";
+
 const WebCam = () => {
-  return <div>WebCam</div>;
+  const camElement = useRef<HTMLVideoElement | null>(null);
+  const streamWebCam = async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+
+    if (camElement.current) {
+      camElement.current.srcObject = stream;
+      await camElement.current?.play();
+    }
+  };
+  useEffect(() => {
+    streamWebCam();
+  }, []);
+
+  return (
+    <video
+      ref={camElement}
+      className="w-32 h-32 draggable object-cover rounded-full aspect-video border-2 relative border-white"
+    >
+      WebCam
+    </video>
+  );
 };
 
 export default WebCam;
